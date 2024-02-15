1bbf5c1a56a8e1a93b3b401005a1774557072456
Woo-yeong Kim <kimwy1997@gmail.com>
2024-02-15 13:13:56 +0900

    [BuILDY-22] add members

--- dist/index.js
+++ dist/index.js
@@ -46472,7 +46472,14 @@ exports.getSlackClient = getSlackClient;
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.COLORS = exports.MEMBERS = void 0;
-exports.MEMBERS = { w00ing: 'U02U5KJ3G7P' };
+exports.MEMBERS = {
+    w00ing: 'U02U5KJ3G7P',
+    pebblepark: 'U04DT95LT8V',
+    gwjang: 'U02U5KJ3G7P',
+    opencho: 'U01JL0MJR2P',
+    JYC11: 'U02U5KJ3G7P',
+    jjjwodls: 'U04FGV58GE7'
+};
 exports.COLORS = {
     SUCCESS: '#2EB67D',
     PENDING: '#FFD166',
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,11 @@
-export const MEMBERS: Record<string, string> = { w00ing: 'U02U5KJ3G7P' }
+export const MEMBERS: Record<string, string> = {
+  w00ing: 'U02U5KJ3G7P',
+  pebblepark: 'U04DT95LT8V',
+  gwjang: 'U02U5KJ3G7P',
+  opencho: 'U01JL0MJR2P',
+  JYC11: 'U02U5KJ3G7P',
+  jjjwodls: 'U04FGV58GE7'
+}
 
 export const COLORS = {
   SUCCESS: '#2EB67D',
